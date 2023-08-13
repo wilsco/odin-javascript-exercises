f4071b593825b46be9b1fdbe5a3331e3b9253f8d
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-12 21:31:03 -0400

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let new_str = "";
+    let i = str.length -1;
+    for (i; i >= 0; i--) {
+        new_str += str.charAt(i);
+    }
+    return new_str
 };
 
 // Do not edit below this line
