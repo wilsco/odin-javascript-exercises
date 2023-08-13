cbb405ffc2927f136cddf2ce1f9230be0a736770
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-12 21:15:10 -0400

    Complete exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) return 'ERROR'
+    for (let i=1; i<=num; i++) {
+        result += string;
+    }
 
+    return result
 };
 
 // Do not edit below this line
