276feac348fc332386d3da47868c1c6600f4119b
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-13 17:48:50 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+ if (!(Number.isInteger(a) && Number.isInteger(b))) return "ERROR"
+ if (a < 0 || b < 0) return "ERROR"
+  const bigNumber = Math.max(a,b);
+  const smallNumber = Math.min(a,b);
+ let sum = 0;
+ for (let i=smallNumber; i<= bigNumber; i++) {
+    sum += i;
+ }
+  return sum;
 };
 
 // Do not edit below this line
