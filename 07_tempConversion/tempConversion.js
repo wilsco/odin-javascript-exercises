cfd7f4fe56d230dddf5799dc83c2db34886b2f81
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-13 18:01:29 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(f) {
+  let c = f * 9 / 5 +32;
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(c) {
+  let f = (c - 32) * 5/9 ;
+  return Math.round(f*10) / 10;
 };
 
 // Do not edit below this line
