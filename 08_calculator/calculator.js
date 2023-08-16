975c3b8db23189f971c9a2ad528328aebbd3680f
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-16 16:12:14 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
 const add = function() {
-	
+	return arguments[0]+arguments[1]
 };
 
 const subtract = function() {
-	
+	return arguments[0]-arguments[1]
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const sum = arr.reduce((sum,a) => sum +a, 0);
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	const m = arr.reduce((p,a) => p*a, 1);
+  return m
 };
 
 const power = function() {
-	
+	return arguments[0]**arguments[1]
 };
 
 const factorial = function() {
-	
+	let i = arguments[0];
+  let answer = 1;
+  while (i>0) {
+    answer *= i--;
+  }
+  return answer
 };
 
 // Do not edit below this line
