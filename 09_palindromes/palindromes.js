12b037e2e634a650e52b7856c15c909297116cbd
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-16 16:35:09 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+//remove punctuation, spaces, caps
+  let s = input.toLowerCase().split("");
+  s = s.filter(c => !punctuation.includes(c));
+  let original = s.join("");
+  let reversed = s.reverse().join("");
+  return reversed === original
 };
+
+const punctuation = [',',' ','.','!'];
 
 // Do not edit below this line
 module.exports = palindromes;
