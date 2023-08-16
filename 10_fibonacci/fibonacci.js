c5adc7cfd0a2e7febe2fa951ed58ddd439d85015
Will Scott <70453693+wilsco@users.noreply.github.com>
2023-08-16 16:46:41 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(a) {
+  if(a<0) return "OOPS"
+  let n = parseInt(a);
+  if (n===1 || n===2) {
+    return 1
+  } else return fibonacci(n-1) +fibonacci(n-2)
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
